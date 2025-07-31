c5e3f01e9024b5fa362486f5d5e2cfbbd7069296
zarlixq <ahmetselimtuna@gmail.com>
2025-07-31 03:35:59 +0300

    Redirect root (/) to /register

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,9 @@ export default function Home() {
   const router = useRouter()
 
   useEffect(() => {
-    router.push('/register') // replace yerine push
-  }, [])
+    // Sayfa yüklendiğinde register sayfasına yönlendir
+    router.replace('/register')
+  }, [router])
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <h1 className="text-white text-2xl">Yönlendiriliyor...</h1>
-    </div>
-  )
+  return null
 }
